f7f910b304636313ecf8e9dd1da5d9b46ed675ca
kevin flores <kevindannerflores@gmail.com>
2025-10-07 22:32:08 -0400

    docs: se uso- CoreBiz template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Layouts } from './layouts/layouts';
+export const routes: Routes = [
+    {
+        path:'',
+        component:Layouts,
+        children:[
+            {
+                path:'',
+                loadComponent:()=>
+                    import('./home/home').then(m=>m.Home)
+            },
+            {
+                path:'about',
+                loadComponent:()=>
+                    import('./about/about').then(m=>m.About)
+            },
 
-export const routes: Routes = [];
+        ]
+    }
+];
